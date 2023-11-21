79d73a2cf63cb144aa8c6f0c0c59af04baddd80a
shamilkvv <shamiltv803@gmail.com>
2023-11-21 19:37:00 +0400

    count the number of each letter

--- unordered map/unordered map.cpp
+++ unordered map/unordered map.cpp
@@ -1,1 +1,9 @@
-﻿
+﻿#include <iostream>
+#include <unordered_map>
+
+bool MakeupWord(const std::string& randomLetter, const std::string& paper) {
+    std::unordered_map<char, int> letterCount;
+
+    for (auto c : paper) {
+        letterCount[c]++;
+    }
